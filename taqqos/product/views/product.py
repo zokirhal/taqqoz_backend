5bfb29d8a3b3bed6753fca3250af350ab0d0b8ed
Elshod Allayorov <elshodallayarov@gmail.com>
2024-01-09 23:00:48 +0500

    product attribute filter added

--- taqqos/product/views/product.py
+++ taqqos/product/views/product.py
@@ -1,7 +1,8 @@
+from django.db.models import Q
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from taqqos.product.filters import ProductFilter, ProductPriceFilter
-from taqqos.product.models import Product, ProductPrice
+from taqqos.product.models import Product, ProductPrice, ProductAttribute, Option, Attribute
 from taqqos.product.serializers.product import ProductSerializer, ProductPriceSerializer
 
 
@@ -10,6 +11,27 @@ class ProductViewSet(ReadOnlyModelViewSet):
     serializer_class = ProductSerializer
     filterset_class = ProductFilter
     search_fields = ("name_uz", "name_ru")
+
+    def get_queryset(self):
+        query_params = dict(self.request.query_params)
+        for field in self.filterset_class.Meta.fields:
+            query_params.pop(field, "")
+        product_attributes = []
+        for key, val in query_params.items():
+            key = key.split("_")
+            option = Option.objects.filter(Q(attribute__code=key[0]) & Q(code=key[1])).first()
+            p_atts = []
+            if option:
+                if option.attribute.type == Attribute.TEXT:
+                    if option.code == "from":
+                        p_atts = ProductAttribute.objects.filter(attribute=option.attribute, option__vaue__gte=val)
+                    if option.code == "to":
+                        p_atts = ProductAttribute.objects.filter(attribute=option.attribute, option__vaue__lte=val)
+                else:
+                    p_atts = ProductAttribute.objects.filter(option=option)
+                product_attributes.extend(p_atts)
+        qs = self.filter_queryset(self.queryset).filter(attributes__in=product_attributes).distinct()
+        return qs
 
 
 class ProductPriceViewSet(ReadOnlyModelViewSet):
